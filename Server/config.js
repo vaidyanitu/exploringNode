6e1daae6f963a4e726e03d72c50cc6efa9d26a25
vaidyanitu <33897185+vaidyanitu@users.noreply.github.com>
2019-09-27 12:43:06 +0545

    Create config.js

---
+++ Server/config.js
@@ -0,0 +1,6 @@
+module.exports={
+    'url' :  // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+    'secret'://your secret here,   
+    'clientID'://your google client id ,
+    'clientSecret'://your google client secret
+}
